de7f634016121f57d18ed65933e217f4e6c42997
Shane Wilton <shane.wilton@gmail.com>
2020-02-25 22:56:11 -0800

    Add tag filtering

--- lib/keter/blog.ex
+++ lib/keter/blog.ex
@@ -1,6 +1,10 @@
 defmodule Keter.Blog do
   alias Keter.Blog.Post
 
+  defmodule NotFoundError do
+    defexception [:message, plug_status: 404]
+  end
+
   for app <- [:earmark, :makeup_elixir] do
     Application.ensure_all_started(app)
   end
@@ -17,8 +21,27 @@ defmodule Keter.Blog do
     end
 
   @posts Enum.sort_by(posts, & &1.date, {:desc, Date})
+  @tags posts |> Enum.flat_map(& &1.tags) |> Enum.uniq() |> Enum.sort()
+
+  def list_tags do
+    @tags
+  end
 
   def list_posts do
     @posts
+  end
+
+  def get_posts_by_tag!(tag) do
+    case Enum.filter(list_posts(), &(tag in &1.tags)) do
+      [] -> raise NotFoundError, "posts with tag=#{tag} not found"
+      posts -> posts
+    end
+  end
+
+  def get_post_by_id!(id) do
+    case Enum.find(list_posts(), fn post -> post.id == id end) do
+      nil -> raise NotFoundError, "post with id=#{id} not found"
+      post -> post
+    end
   end
 end
